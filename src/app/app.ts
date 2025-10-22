bf586b4f140b299c9ef6fd6c28a79ba3633ec9af
Timur Bukin <bukintimur4@gmail.com>
2025-10-22 21:49:23 +0300

    title1.1

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Header} from './components/header/header';
 import {AppButton} from './components/app-button/app-button';
+import {Title} from './components/title/title';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, AppButton],
+  imports: [RouterOutlet, Header, AppButton, Title],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
